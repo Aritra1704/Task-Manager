9db64072bca7ecc988792cf29f54811e5a783853
Aritra1704 <aritra1704@gmail.com>
2019-07-15 21:53:49 +0530

    Added querying completed,limit, skip and sortBy in GET tasks api

--- src/routers/task.js
+++ src/routers/task.js
@@ -25,12 +25,20 @@ router.post('/tasks', auth, async (req, res) => {
     // })
 })
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdAt_asc   or createdAt_desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
-
+    const sort = {}
     if(req.query.completed) {
         match.completed = req.query.completed === 'true'
     } 
+
+    if(req.query.sortBy) {
+        const parts = req.query.sortBy.split('_')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
 
     try {
         // const task = await Task.find({owner: req.user._id})
@@ -38,7 +46,12 @@ router.get('/tasks', auth, async (req, res) => {
         // await req.user.populate('tasks').execPopulate()
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
         }).execPopulate()
         res.status(201).send(req.user.tasks)
     } catch(e) {
