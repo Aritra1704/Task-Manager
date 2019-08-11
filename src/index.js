c73dcb8766b8031f1db299eff524164e0829c395
Aritra1704 <aritra1704@gmail.com>
2019-08-11 20:05:12 +0530

    Adding testing modules

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-const express = require('express')
-require('./db/mongoose')
-
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+// const express = require('express')
+// require('./db/mongoose')
+
+// const userRouter = require('./routers/user')
+// const taskRouter = require('./routers/task')
+
+// const app = express()
+// const port = process.env.PORT
+
+// app.use(express.json())
+// app.use(userRouter)
+// app.use(taskRouter)
+
+
 
 
 // app.use((req, res, next) => {
---
+++ test/user.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest')
+const app = require('../src/app')
+
+test('Should signup a new user', async () => {
+    await request(app).post('./users').send({
+        name: 'Artitra',
+        email: 'aritra@test.com',
+        password: '12346Aa'
+    }).expect(201)
+})
