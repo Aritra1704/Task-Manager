de0d4bfd418d896000a3cbef65fe176e08230a5c
Aritra1704 <aritra1704@gmail.com>
2019-08-13 12:49:39 +0530

    Resolved testing jest issue

--- test/user.test.js
+++ test/user.test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../src/app')
 
 test('Should signup a new user', async () => {
-    await request(app).post('./users').send({
+    await request(app).post('/users').send({
         name: 'Artitra',
         email: 'aritra@test.com',
         password: '12346Aa'
